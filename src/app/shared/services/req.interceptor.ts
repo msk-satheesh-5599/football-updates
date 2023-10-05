8ec1db7ad06814eb4bf1fe8d3d448fac8160f78b
Satheeshkumar Muthusamy <s.muthusamy@soprasteria.com>
2023-10-06 01:45:40 +0530

    Ignoring enviroments

--- src/app/shared/services/req.interceptor.ts
+++ src/app/shared/services/req.interceptor.ts
@@ -1,12 +1,12 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpInterceptor,
-  HttpRequest,
-} from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable()
 export class ReqInterceptor implements HttpInterceptor {
