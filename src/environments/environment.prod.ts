4da2390154e35d346cb014ec51df3f1e87312442
Satheeshkumar Muthusamy <s.muthusamy@soprasteria.com>
2023-10-06 00:56:23 +0530

    changes done for football-updates application

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   host: 'v3.football.api-sports.io',
-  apiKey: 'bcfac433b77a7d47b38e051a3680b2f8',
+  apiKey: 'f3dd13dd0f54862da12e36ce613fca30',
   endpoint: 'https://v3.football.api-sports.io/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   host: 'v3.football.api-sports.io',
-  apiKey: 'bcfac433b77a7d47b38e051a3680b2f8',
+  apiKey: 'f3dd13dd0f54862da12e36ce613fca30',
   endpoint: 'https://v3.football.api-sports.io/',
 };
 
